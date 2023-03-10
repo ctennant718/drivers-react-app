5816b7905161dd0a7b0317df48e590248ac60256
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-03-10 12:23:08 +0000

    added MUI and theme

---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+import green from "@mui/material/colors/green";
+import grey from "@mui/material/colors/grey";
+// import green from '@mui/material/colors/green';
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: green[500],
+      contrastText: grey[900],
+    },
+    secondary: {
+      main: "#edf2ff",
+      contrastText: grey[900],
+    },
+  },
+});
+
+export default theme;
