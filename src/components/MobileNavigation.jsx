1d5c758102a83dfcc86dcad77be85869821e3062
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-03-11 11:21:29 +0000

    UI navigation added

---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+
+function MobileNavigation({
+  mobileOpen = false,
+  handleDrawerToggle = () =>
+    console.log("No handleDrawerToggle function provided."),
+  drawerWidth = 240,
+}) {
+  return (
+    <Box component="nav">
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            My Dogs
+          </Typography>
+          <Divider />
+          <List>
+            <ListItem>
+              <ListItemButton
+                sx={{ textAlign: "left" }}
+                component={NavLink}
+                to="/"
+              >
+                <ListItemText primary={"My Dogs"} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton
+                sx={{ textAlign: "left" }}
+                component={NavLink}
+                to="/add"
+              >
+                <ListItemText primary={"Add a Dog"} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+}
+
+export default MobileNavigation;
